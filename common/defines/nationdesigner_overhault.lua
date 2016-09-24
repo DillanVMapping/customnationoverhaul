2d4a873381a04953a6f7183a37bb17f38ea5688d
xqanf <xqanf@hotmail.com>
2016-09-24 19:17:34 +0300

    custom nation ideas
    and some more

---
+++ common/defines/nationdesigner_overhault.lua
@@ -0,0 +1,26 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NNationDesigner.POINTS_AVAILABLE = 1600
+NDefines.NNationDesigner.RULER_BASE_AGE = 30
+NDefines.NNationDesigner.RULER_MIN_AGE = 0
+NDefines.NNationDesigner.RULER_MAX_AGE = 100
+NDefines.NNationDesigner.HEIR_BASE_AGE = 15
+NDefines.NNationDesigner.HEIR_MIN_AGE = 0
+NDefines.NNationDesigner.HEIR_MAX_AGE = 60
+NDefines.NNationDesigner.RULER_BASE_SKILL = 2
+NDefines.NNationDesigner.RULER_MAX_SKILL = 6
+NDefines.NNationDesigner.RULER_SKILL_COST_MODIFIER = 0.5
+NDefines.NNationDesigner.BASE_TAX_COST_MODIFIER = 0.5
+NDefines.NNationDesigner.MANPOWER_COST_MODIFIER = 0.5
+NDefines.NNationDesigner.GOLD_PROVINCE_COST_MULTIPLIER = 2
+NDefines.NNationDesigner.TECH_GROUP_COST_MODIFIER = 50
+NDefines.NNationDesigner.MEMBER_OF_HRE_COST = 20
+NDefines.NNationDesigner.MEMBER_OF_HRE_PROVINCE_COST = 10
+NDefines.NNationDesigner.TECH_GROUP_NO_NEIGHBOUR_COST = 50
+NDefines.NNationDesigner.IDEAS_MAX_LEVEL = 5
+NDefines.NNationDesigner.FLAT_TAX_AMOUNT = 4
+NDefines.NNationDesigner.FLAT_MP_AMOUNT = 3
+NDefines.NNationDesigner.MAX_DISTANCE_TO_OWNER_AREA = 4000
+NDefines.NNationDesigner.MAX_DISCOVERED_PROVINCE_DISTANCE = 1000
